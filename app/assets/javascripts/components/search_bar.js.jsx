fe03f53dd7879da7f8c081408520c6cc767b5ceb
Ami Hays <amihays2@gmail.com>
2015-11-23 18:46:56 -0800

    Cleaned up string matching with regular expression

--- app/assets/javascripts/components/search_bar.js.jsx
+++ app/assets/javascripts/components/search_bar.js.jsx
@@ -33,73 +33,82 @@ window.SearchBar = React.createClass({
   },
 
   _calculateSuggestedDiagnoses: function (topDiagnoses) {
-    return topDiagnoses.concat(this._relevantDiagnoses(10 - topDiagnoses.length));
+    if (this.state.query === "") {
+      return [];
+    } else {
+      return topDiagnoses.concat(this._relevantDiagnoses(10 - topDiagnoses.length));
+    }
   },
 
   _relevantDiagnoses: function (count) {
-    var allDiagnoses = DiagnosisStore.all();
-    // var relevanceScores = this._computeRelevance(allDiagnoses);
-    var numSearchWords = this.state.query.split(/[ -]/).length;
-    // console.log(relevanceScores);
-    var resultCounts = [];
-    allDiagnoses.forEach(function(diagnosis) {
-      var searchRelevance = this._searchRelevance(diagnosis);
-      // console.log(diagnosis, searchRelevance);
-      if (searchRelevance >= numSearchWords) {
-        var added = false;
-        for (var i = 0; i < resultCounts.length; i++) {
-          if (resultCounts[i][1] < searchRelevance) {
-            resultCounts.splice(i, 0, [diagnosis, searchRelevance]);
-            added = true;
-            break;
-          }
-        }
-        if (!added && resultCounts.length < count && searchRelevance >= numSearchWords) {
-          resultCounts.push([diagnosis, searchRelevance]);
-        } else if (resultCounts.length > count) {
-          resultCounts = resultCounts.slice(0, count)
-        }
+    var searchWords = this.state.query.split(/[ -]/);
+    var regExpString = "";
+    searchWords.forEach(function(word) {
+      regExpString += "(?=.* " + word + "|^" + word
+      if (COMMON_ABBREVS[word]) {
+        regExpString += "|.* " + COMMON_ABBREVS[word] + "|^" + COMMON_ABBREVS[word];
+      }
+      regExpString += ")";
+    });
+    var searchRegExp = new RegExp(regExpString);
+    var results = [];
+    DiagnosisStore.all().forEach(function(diagnosis) {
+      if (diagnosis.search(searchRegExp) >= 0) {
+          results.push(diagnosis);
       }
     }.bind(this));
-    return resultCounts.map(function(result) { return result[0]; });
+    return results.slice(0, 5);
   },
-  //
-  // _computeRelevance: function (diagnoses) {
-  //   var relevanceScores = {};
-  //   diagnoses.forEach(function (diagnosis) {
-  //     var relevance = this._searchRelevance(diagnosis);
-  //     relevanceScores[diagnosis] = relevance;
-  //   }.bind(this))
-  //   return relevanceScores;
+
+  // _relevantDiagnoses: function (count) {
+  //   var numSearchWords = this.state.query.split(/[ -]/).length;
+  //   var resultCounts = [];
+  //   DiagnosisStore.all().forEach(function(diagnosis) {
+  //     var searchRelevance = this._searchRelevance(diagnosis);
+  //     if (searchRelevance >= numSearchWords) {
+  //       var added = false;
+  //       for (var i = 0; i < resultCounts.length; i++) {
+  //         if (resultCounts[i][1] < searchRelevance) {
+  //           resultCounts.splice(i, 0, [diagnosis, searchRelevance]);
+  //           added = true;
+  //           break;
+  //         }
+  //       }
+  //       if (!added && resultCounts.length < count && searchRelevance >= numSearchWords) {
+  //         resultCounts.push([diagnosis, searchRelevance]);
+  //       } else if (resultCounts.length > count) {
+  //         resultCounts = resultCounts.slice(0, count)
+  //       }
+  //     }
+  //   }.bind(this));
+  //   return resultCounts.map(function(result) { return result[0]; });
   // },
-
-  _searchRelevance: function (diagnosis) {
-    var searchWords = this.state.query.split(/[ -]/);
-    var sum = 0;
-    searchWords.forEach(function(word) {
-      sum += this._wordRelevance(diagnosis, word);
-    }.bind(this))
-    return sum;
-  },
-
-  _wordRelevance: function (diagnosis, word) {
-    if (word === "") {
-      return 0;
-    }
-    var wordRegex = new RegExp('^' + word.toLowerCase());
-    var abbrevRegex = COMMON_ABBREVS[word] ? new RegExp('^' + COMMON_ABBREVS[word]) : null;
-    var wordRelevance = 0;
-    diagnosis.split(/[ -]/).forEach(function(diagnosisWord) {
-      if (abbrevRegex && diagnosisWord.toLowerCase().match(abbrevRegex)) {
-        wordRelevance += 1.01;
-      } else if (diagnosisWord.toLowerCase().match(wordRegex)) {
-        wordRelevance += 1;
-      } else {
-        wordRelevance += 0;
-      }
-    });
-    return wordRelevance;
-  },
+  //
+  // _searchRelevance: function (diagnosis) {
+  //   var searchWords = this.state.query.split(/[ -]/);
+  //   var sum = 0;
+  //   searchWords.forEach(function(word) {
+  //     sum += this._wordRelevance(diagnosis, word);
+  //   }.bind(this))
+  //   return sum;
+  // },
+  //
+  // _wordRelevance: function (diagnosis, word) {
+  //   if (word === "") {
+  //     return 1;
+  //   }
+  //   var wordRegex = new RegExp('^' + word.toLowerCase());
+  //   var abbrevRegex = COMMON_ABBREVS[word] ? new RegExp('^' + COMMON_ABBREVS[word]) : null;
+  //   var wordRelevance = 0;
+  //   diagnosis.split(/[ -]/).forEach(function(diagnosisWord) {
+  //     if (abbrevRegex && diagnosisWord.toLowerCase().match(abbrevRegex)) {
+  //       wordRelevance = 1.1;
+  //     } else if (diagnosisWord.toLowerCase().match(wordRegex)) {
+  //       wordRelevance = wordRelevance > 1 ? wordRelevance : 1;
+  //     }
+  //   });
+  //   return wordRelevance;
+  // },
 
   render: function () {
     return(
